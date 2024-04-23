6953df2dc9491786caf0219cc769f1488b5bff4c
Aaron Pribadi <aaron.pribadi@gmail.com>
2024-04-22 17:57:17 -0700

    z

--- tests/all.rs
+++ tests/all.rs
@@ -35,32 +35,24 @@ fn test_api() {
 
 #[test]
 fn test_special_traits() {
-  // fn is_clone<T: Clone>() {}
-  // fn is_copy<T: Copy>() {}
-  // fn is_eq<T: Eq>() {}
-  // fn is_hash<T: std::hash::Hash>() {}
-  // fn is_ord<T: Ord>() {}
-  // fn is_partial_eq<T: PartialEq>() {}
-  // fn is_partial_ord<T: PartialOrd>() {}
-  fn is_ref_unwind_safe<T: std::panic::RefUnwindSafe>() {}
   fn is_send<T: Send>() {}
   fn is_sync<T: Sync>() {}
-  // fn is_unpin<T: Unpin>() {}
   fn is_unwind_safe<T: std::panic::UnwindSafe>() {}
+  fn is_ref_unwind_safe<T: std::panic::RefUnwindSafe>() {}
 
-  is_ref_unwind_safe::<Store>();
   is_send::<Store>();
   is_sync::<Store>();
   is_unwind_safe::<Store>();
+  is_ref_unwind_safe::<Store>();
 
   is_send::<Arena<'static>>();
-  is_ref_unwind_safe::<Arena<'static>>();
   is_unwind_safe::<Arena<'static>>();
+  is_ref_unwind_safe::<Arena<'static>>();
 
-  is_ref_unwind_safe::<Slot<'static, u64>>();
   is_send::<Slot<'static, u64>>();
   is_sync::<Slot<'static, u64>>();
   is_unwind_safe::<Slot<'static, u64>>();
+  is_ref_unwind_safe::<Slot<'static, u64>>();
 }
 
 #[test]
